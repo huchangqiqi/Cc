8cdf9d24532bc08749ff20858030c88931b248c8
spike <huchangqiqi@gmail.com>
2016-11-21 18:49:07 +0000

    mod pg.sql

--- pgsql/pq.sql
+++ pgsql/pq.sql
@@ -1,3 +1,19 @@
-create function and_em() returns integer as $$
+#存储过程
+create function and_em(integer,integer) returns integer as $$
 select $1 + $2;
 $$ language sql;
+
+#触发器
+create function select_ts() returns integer as $$
+begin
+if(NEW.date >= 3) then
+insert into ts_audit NEW.name;
+endif;
+end;
+$$ language plpgsql;
+
+create trigger ts_audit
+after update on ts
+for each row execute procedure select_ts();
+
+
